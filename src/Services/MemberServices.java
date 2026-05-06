21199691bfd354a6912d36b6fda3472bb037823d
CompanyProfile <alanood94895@gmail.com>
2026-05-06 10:01:56 +0400

    Add MemberServices Code

--- src/Services/MemberServices.java
+++ src/Services/MemberServices.java
@@ -1,4 +1,78 @@
 package Services;
 
+import Entities.LibraryItem;
+import Entities.Member;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class MemberServices {
+    List<LibraryItem> libraryItems = new ArrayList<>();
+    Scanner scanner = new Scanner(System.in);
+    Member member = new Member();
+    List<Member> members = new ArrayList<>();
+
+    public Member RegisterNewMember(){
+        System.out.println(" ** Registering new Member **");
+        System.out.println("Please Enter Member ID");
+        String newMemberId = scanner.nextLine();
+        member.setMemberID(newMemberId);
+
+        System.out.println("Please Enter Member Name");
+        String newMemberName = scanner.nextLine();
+        member.setMemberName(newMemberName);
+
+        System.out.println("Please Enter Member Phone Number");
+        Integer newMemberPhone = Integer.valueOf(scanner.nextLine());
+        member.setMemberPhone(newMemberPhone);
+
+        System.out.println("Please Enter Member Address");
+        System.out.println("Street:");
+        String  street = scanner.nextLine();
+        member.setStreet(street);
+
+        System.out.println("City:");
+        String  city = scanner.nextLine();
+        member.setCity(city);
+
+        System.out.println("Country:");
+        String  country = scanner.nextLine();
+        member.setCountry(country);
+
+        System.out.println("Postal Code:");
+        Integer  postal = Integer.valueOf(scanner.nextLine());
+        member.setPostalCode(postal);
+
+
+        return null;
+    }
+
+    public List<Member> addNewMember(){
+        Boolean continueFlag = true;
+        while (continueFlag) {
+            members.add(RegisterNewMember());
+            System.out.println("Enter q to exit, press ENTER to continue for more students");
+            if (scanner.nextLine().equalsIgnoreCase("q")) {
+                continueFlag = false;
+            }
+        }
+        return members;
+    }
+
+    public boolean FindMemberByID(){
+        System.out.println("Please enter the member Id you want to find");
+        String searchedMemberID = scanner.nextLine();
+
+        for (int i =0; i< members.size();i++){
+            String target = String.valueOf(members.get(i));
+
+            if (target.equalsIgnoreCase(searchedMemberID)){
+                member.DisplayMemberDetails();
+                return true;
+            }
+        }
+        return false;
+
+    }
 }
