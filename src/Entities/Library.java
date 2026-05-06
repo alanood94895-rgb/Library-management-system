16d7942c185d28e58b575925d3b139db777de29f
CompanyProfile <alanood94895@gmail.com>
2026-05-06 09:41:21 +0400

    Add Library Code

--- src/Entities/Library.java
+++ src/Entities/Library.java
@@ -1,4 +1,53 @@
 package Entities;
 
-public class Library {
+import java.lang.reflect.Member;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Library extends Address {
+    String libraryCode;
+    String libraryName;
+    List<Member> members = new ArrayList<>();
+    List<LibraryItem> libraryItems = new ArrayList<>();
+
+
+    public void setLibraryName(String libraryName) {
+        this.libraryName = libraryName;
+    }
+
+    public void setLibraryCode(String libraryCode) {
+        this.libraryCode = libraryCode;
+    }
+
+    public List<Member> getMembers() {
+        return members;
+    }
+
+    public List<LibraryItem> getLibraryItems() {
+        return libraryItems;
+    }
+
+    public String getLibraryName() {
+        return libraryName;
+    }
+
+    public String getLibraryCode() {
+        return libraryCode;
+    }
+
+    public void setMembers(List<Member> members) {
+        this.members = members;
+    }
+
+    public void setLibraryItems(List<LibraryItem> libraryItems) {
+        this.libraryItems = libraryItems;
+    }
+
+    @Override
+    public String toString() {
+        return "Library{" +
+                "libraryCode='" + libraryCode + '\'' +
+                ", libraryName='" + libraryName + '\'' +
+                '}';
+    }
 }
