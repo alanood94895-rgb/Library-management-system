e564135589da6ee49ff14ab2c39a8ded9e63206b
CompanyProfile <alanood94895@gmail.com>
2026-05-06 09:42:52 +0400

    Add Address code

--- src/Entities/Address.java
+++ src/Entities/Address.java
@@ -1,4 +1,50 @@
 package Entities;
 
 public class Address {
+    String street;
+    String city;
+    Integer postalCode;
+    String country;
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public void setPostalCode(Integer postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public Integer getPostalCode() {
+        return postalCode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "street='" + street + '\'' +
+                ", city='" + city + '\'' +
+                ", postalCode=" + postalCode +
+                ", country='" + country + '\'' +
+                '}';
+    }
 }
